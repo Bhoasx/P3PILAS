5a4569d963ef642566269cd3134ab0507fb3d89f
Leverna <165915947+Bhoasx@users.noreply.github.com>
2025-10-07 08:07:53 -0500

    first commit

---
+++ src/pilasenteros/Pila.java
@@ -0,0 +1,41 @@
+package pilasenteros;
+import java.util.Stack;
+
+public class Pila {
+    private Stack<Integer> pila;
+
+    public Pila (){
+        pila = new Stack<Integer>();
+    }
+
+    public void push(Integer dato){
+        pila.push(dato);
+    }
+
+    public Integer pop() throws Exception {
+        if (pila.isEmpty())
+            throw new Exception("La pila no tiene elementos");
+        return pila.pop();
+    }
+
+    public Integer peek() throws Exception{
+        if (pila.isEmpty())
+            throw new Exception("La pila no tiene elementos");
+        return pila.peek();
+    }
+
+    public int search(Integer i) throws Exception{
+        if (pila.isEmpty())
+            throw new Exception("Pila vacia");
+        return pila.search(i);
+    }
+
+    @Override
+    public String toString() {
+        String mensaje = "Elementos de la pila:\n";
+        for (int i = pila.size() - 1; i >= 0; i--) {
+            mensaje += pila.get(i) + "\n";
+        }
+        return mensaje;
+    }
+}
